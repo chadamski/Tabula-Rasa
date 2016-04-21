170832159362f4a003516100d44cc467a306a6a1
Chelsea Adams <chelsea@chadams.me>
2016-04-21 11:52:55 -0400

    update jQuery

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@ wp_reset_query();
 $templateDirectory = get_bloginfo('template_directory');
 include(locate_template('parts/ui-footer.php'));
 echo '</div>';
-	echo '<script src="'.$templateDirectory.'/assets/js/vendor/jquery-2.2.0.min.js" type="text/javascript"></script>';
+	echo '<script src="'.$templateDirectory.'/assets/js/vendor/jquery-2.2.3.min.js" type="text/javascript"></script>';
 	echo '<script src="'.$templateDirectory.'/assets/js/plugins.js" type="text/javascript"></script>';
 	echo '<script src="'.$templateDirectory.'/assets/js/base.js" type="text/javascript"></script>'; ?>
 	<script type="text/javascript">(function() {
