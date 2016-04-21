b559ea86b24b11b54d0c8c19df2f8e966f1cf422
Chelsea Adams <chelsea@chadams.me>
2016-04-21 11:54:39 -0400

    destroy footer script

--- footer.php
+++ footer.php
@@ -6,22 +6,6 @@ echo '</div>';
 	echo '<script src="'.$templateDirectory.'/assets/js/vendor/jquery-2.2.3.min.js" type="text/javascript"></script>';
 	echo '<script src="'.$templateDirectory.'/assets/js/plugins.js" type="text/javascript"></script>';
 	echo '<script src="'.$templateDirectory.'/assets/js/base.js" type="text/javascript"></script>'; ?>
-	<script type="text/javascript">(function() {
-		if (!window.mc4wp) {
-			window.mc4wp = {
-				listeners: [],
-				forms    : {
-					on: function (event, callback) {
-						window.mc4wp.listeners.push({
-							event   : event,
-							callback: callback
-						});
-					}
-				}
-			}
-		}
-	})();
-	</script>
 	<?php wp_footer();
 	echo '</body>';	
 echo '</html>';
