0ce7791d634b57abdfdbdd2ff3f425bfbdbdd448
Chelsea Adams <chelsea@chadams.me>
2016-04-21 12:49:44 -0400

    added unslider

--- footer.php
+++ footer.php
@@ -4,6 +4,9 @@ $templateDirectory = get_bloginfo('template_directory');
 include(locate_template('parts/ui-footer.php'));
 echo '</div>';
 	echo '<script src="'.$templateDirectory.'/assets/js/vendor/jquery-2.2.3.min.js" type="text/javascript"></script>';
+	echo '<script src="'.$templateDirectory.'/assets/js/vendor/modernizr.js" type="text/javascript"></script>';
+	echo '<script src="'.$templateDirectory.'/assets/js/vendor/three.min.js" type="text/javascript"></script>';
+	echo '<script src="'.$templateDirectory.'/assets/js/vendor/unslider.min.js" type="text/javascript"></script>';
 	echo '<script src="'.$templateDirectory.'/assets/js/plugins.js" type="text/javascript"></script>';
 	echo '<script src="'.$templateDirectory.'/assets/js/base.js" type="text/javascript"></script>'; ?>
 	<?php wp_footer();
