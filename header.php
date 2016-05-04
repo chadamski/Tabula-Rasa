6b0adb0750831a0080ebf569f6ddaeca2d29c7f2
Chelsea Adams <chelsea@chadams.me>
2016-05-04 14:00:09 -0400

    added twitter card support

--- header.php
+++ header.php
@@ -8,8 +8,15 @@ echo '<head>';
 	echo '<meta http-equiv="cleartype" content="on">';
 	echo '<meta name="HandheldFriendly" content="True">';
 	echo '<meta name="MobileOptimized" content="320">';
-	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">';
-	echo '<title>';
+	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">'; ?>
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@ USER NAME HERE">
+	<meta name="twitter:creator" content="@ USER NAME HERE">
+	<meta name="twitter:title" content="<?php echo wp_title( '|', true, 'right' ); bloginfo('name'); ?>">
+	<meta name="twitter:url" content="<?php echo get_permalink(); ?>">
+	<meta name="twitter:description" content="<?php get_the_excerpt(); ?>">
+	<meta name="twitter:image" content="<?php $image = wp_get_attachment_url( get_post_thumbnail_id($post->ID)); echo $image; ?>" alt="" width="800" height="600" />
+	<?php echo '<title>';
 	wp_title( '|', true, 'right' );
 	bloginfo('name');
 	echo '</title>';	
@@ -20,11 +27,10 @@ echo '<head>';
 	echo '<script type="text/javascript">$homeURL="'.$homeURL.'";</script>';
 	include(locate_template('parts/head-google-analytics.php'));
 	wp_head(); 
-echo '</head>';
-echo '<body ';
-body_class();
-echo '>';
+	echo '</head>';
+	echo '<body ';
+	body_class();
+	echo '>';
 	echo '<div id="wrapper">';
-	
 	include(locate_template('parts/ui-header.php'));
 ?>
