a5617e80769cf54814737d47454cdab91b864efc
Chelsea Adams <chelsea@chadams.me>
2016-05-18 12:05:08 -0400

    change demo custom post type to team

--- functions.php
+++ functions.php
@@ -30,82 +30,57 @@ remove_action( 'admin_print_scripts','print_emoji_detection_script');
 	Custom Post Types
 \*------------------------------------*/
 
-// Create 1 Custom Post type for demo called Events
-function event_cpt() {
-		$labels = array(
-			'name'                => 'Events',
-			'singular_name'       => 'Event',
-			'menu_name'           => 'Events',
-			'name_admin_bar'      => 'Events',
-			'parent_item_colon'   => 'Parent Event:',
-			'all_items'           => 'All Events',
-			'add_new_item'        => 'Add New Event',
-			'add_new'             => 'Add New',
-			'new_item'            => 'New Event',
-			'edit_item'           => 'Edit Event',
-			'update_item'         => 'Update Event',
-			'view_item'           => 'View Event',
-			'search_items'        => 'Search Event',
-			'not_found'           => 'Not found',
-			'not_found_in_trash'  => 'Not found in Trash',
-		);
-		$args = array(
-			'label'               => 'events',
-			'description'         => 'Custom post type for event',
-			'labels'              => $labels,
-			'supports'            => array( 'title', 'revisions', 'thumbnail' ),
-			'taxonomies'          => array( 'event-categories' ),
-			'hierarchical'        => false,
-			'public'              => false,
-			'show_ui'             => true,
-			'show_in_menu'        => true,
-			'menu_position'       => 5,
-			'menu_icon'           => 'dashicons-calendar-alt',
-			'show_in_admin_bar'   => true,
-			'show_in_nav_menus'   => false,
-			'can_export'          => true,
-			'has_archive'         => true,
-			'exclude_from_search' => false,
-			'publicly_queryable'  => true,
-			'rewrite'             => array('slug' => 'events','with_front' => false),
-			'capability_type'     => 'post',
-		);
-		register_post_type( 'event', $args );
-	}
-	add_action( 'init', 'event_cpt', 0 );
-	function event_taxonomy() {
-		$labels = array(
-			'name'                       => 'Categories',
-			'singular_name'              => 'Category',
-			'menu_name'                  => 'Categories',
-			'all_items'                  => 'All Categories',
-			'parent_item'                => 'Parent Category',
-			'parent_item_colon'          => 'Parent Category:',
-			'new_item_name'              => 'New Category Name',
-			'add_new_item'               => 'Add New Category',
-			'edit_item'                  => 'Edit Category',
-			'update_item'                => 'Update Category',
-			'view_item'                  => 'View Category',
-			'separate_items_with_commas' => 'Separate categories with commas',
-			'add_or_remove_items'        => 'Add or remove categories',
-			'choose_from_most_used'      => 'Choose from the most used',
-			'popular_items'              => 'Popular Categories',
-			'search_items'               => 'Search Categories',
-			'not_found'                  => 'Not Found',
-		);
-		$args = array(
-			'labels'                     => $labels,
-			'hierarchical'               => true,
-			'public'                     => true,
-			'show_ui'                    => true,
-			'show_admin_column'          => true,
-			'show_in_nav_menus'          => true,
-			'show_tagcloud'              => false,
-			'rewrite'                    => false,
-		);
-		register_taxonomy( 'event-categories', array( 'event' ), $args );
-	}
-	add_action( 'init', 'event_taxonomy', 0 );
+
+function register_team() {
+	$labels = array(
+		'name'                  => _x( 'Team', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Team', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Team', 'text_domain' ),
+		'name_admin_bar'        => __( 'Team', 'text_domain' ),
+		'archives'              => __( 'Team Archives', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Teammates', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Teammate', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Teammate', 'text_domain' ),
+		'edit_item'             => __( 'Edit Teammate', 'text_domain' ),
+		'update_item'           => __( 'Update Teammate', 'text_domain' ),
+		'view_item'             => __( 'View Teammate', 'text_domain' ),
+		'search_items'          => __( 'Search Team', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Team', 'text_domain' ),
+		'description'           => __( 'The Team', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'page-attributes',),
+		'hierarchical'          => true,
+		'public'                => false,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-id',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => false,		
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'team', $args );
+}
+add_action( 'init', 'register_team', 0 );
 
 
 ?>
