82ddccecc88b2b8f0f0c0bc0ab27386a8d3af78d
Chelsea Adams <chelsea@chadams.me>
2016-07-08 12:00:53 -0400

    footer adjustment

--- parts/ui-footer.php
+++ parts/ui-footer.php
@@ -1,11 +1,4 @@
-<?php 
-	include(locate_template('variables.php'));
-?>
+<?php include(locate_template('variables.php')); ?>
 <footer class="<?php echo $listClass ?>">
-		<?php 
-			wp_nav_menu( array(
-			    'menu' => 'Footer Menu'
-			) );
-		?>
-	</nav>
+
 </footer>
