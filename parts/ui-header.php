2523b841c8cf3da0ad147aa0932b629922b6d8ce
Chelsea Adams <chelsea@chadams.me>
2016-07-08 09:34:30 -0400

    header tweak

--- parts/ui-header.php
+++ parts/ui-header.php
@@ -1,6 +1,6 @@
 <?php include(locate_template('variables.php')); ?>
 <header class="<?php echo $listClass ?>">
-	<a href="<?php site_url(); ?>" id="logo"><h1><?php bloginfo( 'name' ); ?></h1></a>
+	<a href="<?php echo site_url(); ?>" id="logo"><h1><?php bloginfo( 'name' ); ?></h1></a>
 	<nav role="main">
 		<?php 
 			wp_nav_menu( array(
