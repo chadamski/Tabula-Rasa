d34ac33ba6dfd890a3c622d599c8445482124f7d
Chelsea Adams <chelsea@chadams.me>
2016-07-22 12:07:19 -0400

    added social link from options page to footer

--- parts/ui-footer.php
+++ parts/ui-footer.php
@@ -1,3 +1,20 @@
 <?php include(locate_template('variables.php')); ?>
 <footer class="<?php echo $listClass ?>">
+	<!--<ul class="social">
+		<?php if( get_field('twitter_url','options') ): ?>
+			<li><a href="<?php the_field('twitter_url','options'); ?>" target="_blank"><i class="icon tw smaller"></i></a></li>
+		<?php endif; ?>
+		<?php if( get_field('trip_advisor_url','options') ): ?>
+			<li><a href="<?php the_field('trip_advisor_url','options'); ?>" target="_blank"><i class="icon trip smaller"></i></a></li>
+		<?php endif; ?>
+		<?php if( get_field('contact_email','options') ): ?>
+			<li><a href="mailto:<?php the_field('contact_email','options'); ?>" target="_blank"><i class="icon email smaller"></i></a></li>
+		<?php endif; ?>
+		<?php if( get_field('instagram_url','options') ): ?>
+			<li><a href="<?php the_field('instagram_url','options'); ?>" target="_blank"><i class="icon ig smaller"></i></a></li>
+		<?php endif; ?>
+		<?php if( get_field('facebook_url','options') ): ?>
+			<li><a href="<?php the_field('facebook_url','options'); ?>" target="_blank"><i class="icon fb smaller"></i></a></li>
+		<?php endif; ?>
+	</ul>-->
 </footer>
