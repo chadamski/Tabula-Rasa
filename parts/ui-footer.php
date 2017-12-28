f3478aa0ef41c0df8fd48631911972e1a0f0f574
Chelsea Adams <chelsea@chadams.me>
2017-12-28 13:34:00 -0500

    updated default social icons to svg sprites

--- parts/ui-footer.php
+++ parts/ui-footer.php
@@ -2,19 +2,22 @@
 <footer class="<?php echo $listClass ?>">
 	<!--<ul class="social">
 		<?php if( get_field('twitter_url','options') ): ?>
-			<li><a href="<?php the_field('twitter_url','options'); ?>" target="_blank"><i class="icon tw smaller"></i></a></li>
+			<li><a href="<?php the_field('twitter_url','options'); ?>" target="_blank"><svg class="icon twitter"><use xlink:href="#twitter"/></svg></a></li>
 		<?php endif; ?>
 		<?php if( get_field('trip_advisor_url','options') ): ?>
-			<li><a href="<?php the_field('trip_advisor_url','options'); ?>" target="_blank"><i class="icon trip smaller"></i></a></li>
+			<li><a href="<?php the_field('trip_advisor_url','options'); ?>" target="_blank"><svg class="icon tripadvisor"><use xlink:href="#tripadvisor"/></svg></a></li>
 		<?php endif; ?>
 		<?php if( get_field('contact_email','options') ): ?>
-			<li><a href="mailto:<?php the_field('contact_email','options'); ?>" target="_blank"><i class="icon email smaller"></i></a></li>
+			<li><a href="mailto:<?php the_field('contact_email','options'); ?>" target="_blank"><svg class="icon envelope"><use xlink:href="#envelope"/></svg></a></li>
 		<?php endif; ?>
 		<?php if( get_field('instagram_url','options') ): ?>
-			<li><a href="<?php the_field('instagram_url','options'); ?>" target="_blank"><i class="icon ig smaller"></i></a></li>
+			<li><a href="<?php the_field('instagram_url','options'); ?>" target="_blank"><svg class="icon instagram"><use xlink:href="#instagram"/></svg></a></li>
 		<?php endif; ?>
 		<?php if( get_field('facebook_url','options') ): ?>
-			<li><a href="<?php the_field('facebook_url','options'); ?>" target="_blank"><i class="icon fb smaller"></i></a></li>
+			<li><a href="<?php the_field('facebook_url','options'); ?>" target="_blank"><svg class="icon facebook"><use xlink:href="#facebook"/></svg></a></li>
+		<?php endif; ?>
+		<?php if( get_field('youtube_url','options') ): ?>
+			<li><a href="<?php the_field('youtube_url','options'); ?>" target="_blank"><svg class="icon youtube-2"><use xlink:href="#youtube-2"/></svg></a></li>
 		<?php endif; ?>
 	</ul>-->
 </footer>
