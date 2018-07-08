f5f2b4d8dded4af63740fd5bef6817702ddd8b4b
Chelsea Adams <chelsea@chadams.me>
2018-07-08 17:01:54 -0400

    took out smooth scrollbar

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,15 +1,10 @@
 // site scripts
+var scrollbar; 
+
 function init(){	
 	$('body').addClass('complete');
 	$('a').smoothScroll();
-	var scrollbar = Scrollbar.init(document.getElementById('wrapper'), {
-		speed: .7,
-		damping: 0.1,
-		renderByPixels: true,
-		syncCallbacks: true,
-		continuousScrolling: false,
-		overscrollEffect: false
-	});
+
 } 
 // fire init on initial load
 $(document).ready(function(){
